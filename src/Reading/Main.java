043125f686cbe6d9ff9b6efb313832c9f719b8cb
f6 <git@bail6.aleeas.com>
2025-12-09 15:33:32 +0100

    9.12.2025 - Čtení souboru

---
+++ src/Reading/Main.java
@@ -0,0 +1,38 @@
+package Reading;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ctiNejmensiZeSouboru("src/Reading/cisla.txt");
+    }
+
+    public static void ctiNejmensiZeSouboru(String soubor){
+        int minimum = Integer.MAX_VALUE;
+        try {
+
+            File file = new File(soubor);
+            System.out.println(file.toString());
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextInt()) {
+                int cislo = scanner.nextInt();
+
+                if (minimum > cislo){
+                    minimum = cislo;
+                }
+            }
+
+            System.out.println("Nejmenší hodnota je: " + minimum);
+
+            scanner.close();
+        } catch ( FileNotFoundException e) {
+            System.out.println("Soubor nenalezen: " + e.getMessage());
+
+        }
+
+    }
+
+}
