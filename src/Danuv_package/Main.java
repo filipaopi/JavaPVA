9b75df80ac6c648e6081a57827b55f44a8c87ae8
f6 <git@bail6.aleeas.com>
2025-12-16 00:54:03 +0100

    15.12.2025 - Příprava s Danem.

---
+++ src/Danuv_package/Main.java
@@ -0,0 +1,10 @@
+package Danuv_package;
+
+public class Main {
+    public static void main(String[] args) {
+        Trida t=new Trida();
+        t.trrrrida();
+
+        
+    }
+}
---
+++ src/Danuv_package/Trida.java
@@ -0,0 +1,36 @@
+package Danuv_package;
+
+import java.util.Scanner;
+
+public class Trida {
+    public void trrrrida() {
+        int a = 1;
+        var b = 1;
+        var c = "Eva";
+        String d= "ahoj";
+        int j = 0;
+
+        int x = a % b;
+        System.out.println(x);
+
+        if(a==b){
+            System.out.println(d);
+        }
+        else{
+            System.out.println(c);
+        }
+        for (int i = 0; i < 2; i++) {
+            System.out.println(d);
+        }
+        while(j < 5){
+            System.out.println(x);
+            j = j + 1;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+
+        System.out.println(str);
+    }
+}
